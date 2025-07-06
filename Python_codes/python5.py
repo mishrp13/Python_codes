0f9c257768f233d04bd6dec47119bf0ae6024601
mishrp13 <prabalsonu1997@gmail.com>
2025-07-06 17:28:59 +0530

    This is the fifth commit

---
+++ Python_codes/python5.py
@@ -0,0 +1,11 @@
+
+# Create a function that accepts two numbers and returns the larger.
+import socket
+
+hostname=socket.gethostname()
+ip_address=socket.gethostbyname(hostname)
+
+print(f"hostname is {hostname}")
+print(f"ip_address is {ip_address}")
+
+
