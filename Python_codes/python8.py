f9213d83fcc1d0ecb98b5dbc32fb62673c71f769
mishrp13 <prabalsonu1997@gmail.com>
2025-07-07 15:33:57 +0530

    new changes

--- Python_codes/python8.py
+++ Python_codes/python8.py
@@ -2,19 +2,20 @@
 
 
 file_path=r"C:\python_automation_scripts\python_codes\prabal.txt"
-old_text="Raj"
-new_text="Yahoo"
+# old_text="Raj"
+# new_text="Yahoo"
 
 
-with open (file_path,'r') as file:
-    content=file.read()
+# with open (file_path,'r') as file:
+#     content=file.read()
 
 
-content=content.replace(old_text,new_text)
+# content=content.replace(old_text,new_text)
 
 
-with open(file_path,'w') as file:
-    file.write(content)
+# with open(file_path,'w') as file:
+#     file.write(content)
 
 
-print(f"The file that contained '{old_text}' is changed with '{new_text}'")
+# print(f"The file that contained '{old_text}' is changed with '{new_text}'")
+
