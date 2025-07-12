cb1163f56e18293db9849d5aedb63b193b5d016d
mishrp13 <prabalsonu1997@gmail.com>
2025-07-12 17:41:28 +0530

    new changes in 22nd file:

--- Python_codes/python22.py
+++ Python_codes/python22.py
@@ -3,14 +3,28 @@
 
 import os
 
-def search_file(filename, search_path):
-    for root, dir, files in os.walk(search_path):
+# def search_file(filename, search_path):
+#     for root, dir, files in os.walk(search_path):
+#         if filename in files:
+#             full_path = os.path.join(root, filename)
+#             print(f"Found: {full_path}")
+#             return full_path
+#     print("File not found.")
+#     return None
+
+# # Example usage
+# search_file("example.txt", r"C:\Users\praba\OneDrive\Documents\ras")
+
+
+def serach_files(filename,search_path):
+    for root,dir,files in os.walk(search_path):
         if filename in files:
-            full_path = os.path.join(root, filename)
-            print(f"Found: {full_path}")
+            full_path=os.path.join(root,filename)
+            print(f"file found :  full path is:  '{full_path}")
             return full_path
-    print("File not found.")
+        
+    print(f"file not found")
     return None
 
-# Example usage
-search_file("example.txt", r"C:\Users\praba\OneDrive\Documents\ras")
+
+serach_files("example.txt",r"C:\Users\praba\OneDrive\Documents\ras")
