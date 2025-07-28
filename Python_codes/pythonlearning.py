2c89dea0e271529d223b7e08d0f3cb552665ddef
mishrp13 <prabalsonu1997@gmail.com>
2025-07-28 19:18:43 +0530

    new changes

--- Python_codes/pythonlearning.py
+++ Python_codes/pythonlearning.py
@@ -2,16 +2,14 @@
 
 # 1st program
 
-print("Hello,World")
-print("what's your name")
+# #print("Hello,World")
+# print("what's your name")
 
-myName=input()
-print("It is good to meet you "+ myName)
-print("The length of your name is")
-print(len(myName))
-print("what's your age")
-myAge=input()
-print("you will be "+str(int(myAge)+20)+ " in 20 years down the line and that is not absurd")
-
-
+# myName=input()
+# print("It is good to meet you "+ myName)
+# print("The length of your name is")
+# print(len(myName))
+# print("what's your age")
+# myAge=input()
+# print("you will be "+str(int(myAge)+20)+ " in 20 years down the line and that is not absurd")
 
