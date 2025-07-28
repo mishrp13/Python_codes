6b33511140dc6d5a6497b8bbcf87bf183b61290f
mishrp13 <prabalsonu1997@gmail.com>
2025-07-28 19:15:15 +0530

    new changes in python file

---
+++ Python_codes/pythonlearning.py
@@ -0,0 +1,17 @@
+
+
+# 1st program
+
+print("Hello,World")
+print("what's your name")
+
+myName=input()
+print("It is good to meet you "+ myName)
+print("The length of your name is")
+print(len(myName))
+print("what's your age")
+myAge=input()
+print("you will be "+str(int(myAge)+20)+ " in 20 years down the line and that is not absurd")
+
+
+
