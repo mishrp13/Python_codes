0f9f0764097b936729ff4e379c948f5eb74983a6
mishrp13 <prabalsonu1997@gmail.com>
2025-07-28 19:30:12 +0530

    new changes

--- Python_codes/pythonlearning.py
+++ Python_codes/pythonlearning.py
@@ -1,8 +1,7 @@
 
-
 # 1st program
 
-# #print("Hello,World")
+# print("Hello,World")
 # print("what's your name")
 
 # myName=input()
@@ -12,4 +11,20 @@
 # print("what's your age")
 # myAge=input()
 # print("you will be "+str(int(myAge)+20)+ " in 20 years down the line and that is not absurd")
+
+while True:
+    print("who are you? ")
+    name=input()
+    if name !="Ramesh":
+        continue
+    print("Hello Ramesh")
+    print("what's your pass")
+    passw=input()
+    if passw=="sword":
+        break
+print("Access granted")
+
+
+
+
 
