71d4231e891e2d7ab8ed7b3760be9f563e016425
mishrp13 <prabalsonu1997@gmail.com>
2025-08-09 17:49:43 +0530

    new changes

--- Python_codes/countdownapp.py
+++ Python_codes/countdownapp.py
@@ -6,11 +6,20 @@ inputList=userInput.split(":")
 goal=inputList[0]
 deadline=inputList[1]
 
-print(datetime.datetime.strptime(deadline,"%d.%m.%Y"))
-print(type(datetime.datetime.strptime(deadline,"%d.%m.%Y")))
+# print(datetime.datetime.strptime(deadline,"%d.%m.%Y"))
+# print(type(datetime.datetime.strptime(deadline,"%d.%m.%Y")))
+
+deadline_date=datetime.datetime.strptime(deadline,"%d.%m.%Y")
+today_date=datetime.datetime.today()
+
 
 #user input will always be treated as string
-#learn python:12.07.2025
+#learn python:12.10.2025
+
+# calculate how many days from now till deadline
+
+print(deadline_date-today_date)
+
 
 
 
