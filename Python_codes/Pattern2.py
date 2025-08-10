98fdb9f4340b3bd1ca096ffd45c84aa93ebbdc14
mishrp13 <prabalsonu1997@gmail.com>
2025-08-10 12:02:15 +0530

    new changes

--- Python_codes/Pattern2.py
+++ Python_codes/Pattern2.py
@@ -2,15 +2,13 @@ class Solution:
     def pattern2(self,n):
         for i in range(n):
             for j in range(i+1):
-                print("*",end="")
-
+                print("*", end=" ")
             print()
 
-    def main(self):
+    def main(slef):
         N=5
         sol=Solution()
         sol.pattern2(N)
 
 if __name__=="__main__":
     Solution().main()
-
