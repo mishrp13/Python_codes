0e3b311013d6c9d51f907b0a1018c3aa57533a32
mishrp13 <prabalsonu1997@gmail.com>
2025-08-15 11:16:17 +0530

    new changes

--- Python_codes/Math2.py
+++ Python_codes/Math2.py
@@ -3,15 +3,14 @@ class Solution:
         countOdd=0
         while(n>0):
             lastDigit=n%10
-            if(lastDigit%2==1):
+            if lastDigit%2==1:
                 countOdd=countOdd+1
             n=n//10
         return countOdd
     
-n=98763
+n=678393333
 sol=Solution()
 print(sol.countOddDigit(n))
-
     
             
      
