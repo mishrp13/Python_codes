88307b1c12393c5b5528fc075813f164189f5a57
mishrp13 <prabalsonu1997@gmail.com>
2025-08-15 11:55:28 +0530

    new changes

--- Python_codes/Arrays/selectionsort.py
+++ Python_codes/Arrays/selectionsort.py
@@ -2,19 +2,18 @@ class Solution:
     def selectionSort(self,nums):
         for i in range(len(nums)-1):
             min_index=i
-
             for j in range(i+1,len(nums)):
                 if nums[j]<nums[min_index]:
                     min_index=j
 
             if min_index!=i:
                 nums[i],nums[min_index]=nums[min_index],nums[i]
-        
+
         return nums
     
-if __name__=="__main__":
-    sol=Solution()
-    nums=[64,45,32,43,67]
-    sorted_nums=sol.selectionSort(nums)
-    print(f"sorted values of array is: {sorted_nums}")
 
+if __name__=="__main__":
+    nums=[45,56,12,32,46]
+    sol=Solution()
+    sorted_list=sol.selectionSort(nums)
+    print(f"sorted values of array is : {sorted_list}")
