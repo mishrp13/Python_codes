3a4affa56f08698d42c13f3b7e44a9b844b8d9ac
mishrp13 <prabalsonu1997@gmail.com>
2025-08-16 11:13:46 +0530

    new changes

--- Python_codes/BasicPython/Pattern13.py
+++ Python_codes/BasicPython/Pattern13.py
@@ -1,9 +1,8 @@
 class Solution:
-    def Pattern12(self,n):
+    def Pattern13(self,n):
         for i in range(1,n+1):
             for j in range(i):
                 print(j+1,end=" ")
-            
 
             for j in range(2*n-2*i):
                 print(" ",end=" ")
@@ -13,11 +12,7 @@ class Solution:
 
             print()
 
-           
-
-            
-
 if __name__=="__main__":
-    N=4
     sol=Solution()
-    sol.Pattern12(N)
+    N=4
+    sol.Pattern13(N)
