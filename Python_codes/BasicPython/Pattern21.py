2ebf2b1ba6928f9dd6b60886dd9abf97b430f918
mishrp13 <prabalsonu1997@gmail.com>
2025-08-18 06:51:02 +0530

    new changes

---
+++ Python_codes/BasicPython/Pattern21.py
@@ -0,0 +1,16 @@
+class Solution:
+    def Pattern21(self,n):
+        for i in range(n):
+            for j in range(n):
+                if(i==0 or j==0 or i==n-1 or j==n-1):
+                    print("*",end=" ")
+                else:
+                    print(" ",end=" ")
+
+
+            print()
+
+if __name__=="__main__":
+    N=4
+    sol=Solution()
+    sol.Pattern21(N)
