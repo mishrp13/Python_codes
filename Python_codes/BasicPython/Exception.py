001a1696174a8c9f154bbf05c44142a729637301
mishrp13 <prabalsonu1997@gmail.com>
2025-09-07 19:28:18 +0530

    new changes

--- Python_codes/BasicPython/Exception.py
+++ Python_codes/BasicPython/Exception.py
@@ -1,11 +1,11 @@
-thinkers=['Plato','Aristotle','Socrates']
-while True:
-    try:
-        thinker=thinkers.pop()
-        print(thinker)
-    except IndexError as e:
-        print("we tried to pop too many thinkers")
-        print(e)
-        break
 
-    
+class Fancy_car():
+
+    wheels = 4
+
+    def drivefast(self):
+        print("driving fast" )
+
+car1 = Fancy_car()
+print(car1.wheels)
+car1.drivefast()
