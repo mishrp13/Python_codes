6ebb2182c3efb0f501fc8c6707966a55db9be2a6
mishrp13 <prabalsonu1997@gmail.com>
2025-09-07 13:11:12 +0530

    new changes

---
+++ Python_codes/BasicPython/Exception.py
@@ -0,0 +1,9 @@
+thinkers=['Plato','Aristotle','Socrates']
+while True:
+    try:
+        thinker=thinkers.pop()
+        print(thinker)
+    except IndexError as e:
+        print("we tried to pop too many thinkers")
+        print(e)
+        break
