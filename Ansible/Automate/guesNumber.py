f9f1d4c236acf6e37eb8f9143c5ac3dd5cfeac6a
mishrp13 <prabalsonu1997@gmail.com>
2025-10-03 19:31:19 +0530

    new changes

--- Ansible/Automate/guesNumber.py
+++ Ansible/Automate/guesNumber.py
@@ -1,17 +1,23 @@
-class Solution:
-    def Pattern1(self,n):
-        for i in range(n):
-            for j in range(n):
-                print("*", end=" ")
-            print()
+import random
 
-    def main(self):
-        n=5
-        sol=Solution()
-        sol.Pattern1(n)
+print("Hello, what is your name?. ")
+name=input()
 
-if __name__=="__main__":
-    Solution().main()
+print("well "+ name + "I am thinking of number between 1 and 20")
+secretNumber=random.randint(1,20)
 
+for guesstaken in range(1,7):
+    print("Take a guess")
+    guess=int(input())
 
+    if guess < secretNumber:
+        print("Your guess is too low")
+    elif guess > secretNumber:
+        print("your guess is too high")
+    else:
+        break
 
+if guess == secretNumber:
+    print("Good job " + name + " you guessed my number in "+ str(guesstaken))
+else:
+    print("Nope. The number i was thinking "+str(secretNumber))
